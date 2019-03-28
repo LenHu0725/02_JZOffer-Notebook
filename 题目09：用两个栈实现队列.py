393a0581d78d587135357d2785ba5cf070a18e35
Hoo <zeroice7@163.com>
2019-03-28 21:59:16 +0800

    Create 题目09：用两个栈实现队列.py

---
+++ 题目09：用两个栈实现队列.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+"""
+名称：用两个栈实现队列
+题目：用两个栈实现一个队列，队列声明如下:
+     请实现它的两个函数appendTail和deleteHead，分别完成在队列尾部插入节点和在队列头部删除节点的功能；
+     [理解：用两个栈实现一个队列，完成队列的Push和Pop，队列中元素为int]
+测试：ß
+"""
+
+# 解题思路：
+# 两个栈
+
+class Solution:
+    def __init__(self):
+        self.stackA=[]
+        self.stackB=[]
+
+    def push(self, node):
+        pass
+
+    def pop(self, node):
+        pass
