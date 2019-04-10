aa8c151f8d05667516dd6de2eb5dbefb2883bf50
Hoo <zeroice7@163.com>
2019-04-10 20:22:18 +0800

    题目12：矩阵中的路径

--- 题目12：矩阵中的路径.py
+++ 题目12：矩阵中的路径.py
@@ -7,28 +7,57 @@
 """
 
 class Solution:
-    def searchPath(self,):
+    def hasPath(self, matrix, rows, cols, path):
         """ 
-        查找是否存在某个字符路径 
+        查找 matrix[row, col]下一个 是否为 strs[i]的字符
         输入：
-        - 
+        - matrix:  输入数组;
+        - row:  选定行;
+        - col:  选定列;
+        - s:    下一个字符;
         输出：
-        - 
+        - next: -1未找到下一个字符 1上 2右 3下 4左
         """
-        pass
+        if not matrix or rows<0 or cols<0 or path==None:
+            return False
 
-    def hasPath(self,):
-        """ 
+        markMatrix = [0]*(rows*cols)
+        pathIndex = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if self.hasPathCore(matrix, rows, cols, row, col, path, pathIndex, markMatrix):
+                    return True
+
+        return False
+
+    def hasPathCore(self, matrix, rows, cols, row, col, path, pathIndex, markMatrix):
+        """
         查找字符序列起点
         输入：
-        - 
+        - matrix: 输入矩阵
+        - rows: 所有行数
+        - cols: 所有列数
+        - row:  索引行数
+        - col:  索引列数
+        - path: 路径字符
+        - pathIndex: 路径字符序号
+        - markMatrix: 路径标记
         输出：
-        - 
+        - Bool: 是否存在路径
         """
-        pass
+        if pathIndex == len(path):
+            return True
 
+        if row>=0 and row<rows and col>0 and col<cols and matrix[row*cols+col]==path[pathIndex] and not markMatrix[row*cols+col]:
+            pathIndex+=1
+            markMatrix[row*cols+col] = True
+            hasPath = self.hasPathCore(matrix, rows, cols, row+1, col, path, pathIndex, markMatrix) or \
+                      self.hasPathCore(matrix, rows, cols, row-1, col, path, pathIndex, markMatrix) or \
+                      self.hasPathCore(matrix, rows, cols, row, col+1, path, pathIndex, markMatrix) or \
+                      self.hasPathCore(matrix, rows, cols, row, col-1, path, pathIndex, markMatrix)
+            if not hasPath:
+                pathIndex -=1
+                markMatrix[row*cols+col]=False
 
-arr = [['a','b','t','g'],
-       ['c','f','c','s'],
-       ['j','d','e','h']]
-a = Solution()
+        return hasPath
